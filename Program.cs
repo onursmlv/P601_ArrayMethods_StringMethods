7faa5404c68bf516a6e93d399c665dbb42368b14
0nurkt <onur.ismayilov@kibrit.tech>
2024-03-11 23:39:06 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Linq;
+
+namespace String_Methods__Array_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //int a = 10;
+            //int b= 20;
+            //int c = 30;
+            //string word = "salam";
+
+            //int[] nums = {10, 20 ,30 };
+
+            //string[] words = { "Salam", "Garage"};
+
+            //foreach (int num in nums)
+            //{
+            //    Console.WriteLine(num);
+            //}
+
+            //foreach(string i in words)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //Console.WriteLine(words);
+
+            //Console.WriteLine(nums);
+
+            //Console.WriteLine(nums[3]);
+
+
+            //for (int i = 0; i<nums.Length; i++)
+            //{
+            //    Console.WriteLine(nums);
+            //}
+
+
+
+            //Console.WriteLine(word.IndexOf('o',5));
+            //Console.WriteLine(word.LastIndexOf('o'));
+            //Console.WriteLine(word.LastIndexOf('W',word.Length - 4));
+
+            //Console.WriteLine(word.Length);
+            //Console.WriteLine(word.Replace("Hello","Salam"));
+            //Console.WriteLine(word.Replace("Hello", "Salam", false));
+
+
+            //Console.WriteLine(word.ToLower());
+            //Console.WriteLine(word.ToUpper());
+            //Console.WriteLine(word);
+            //Console.WriteLine(word.Length);
+            //Console.WriteLine(word1.Length);
+
+            //Console.WriteLine(word.Length);
+
+            //string[] strings = { "C++", "Java", "C#" };
+
+            //Console.WriteLine(String.Join(',',strings));
+
+            //string name = "Onur";
+            //string surName = "Ismailov";
+
+            //string fullName = String.Format("{0} Hello  {1}", name, surName);
+
+            //Console.WriteLine(fullName);
+
+            //Console.WriteLine(word.StartsWith("hel"));
+
+            //Console.WriteLine(word.Equals(word2));
+            //Console.WriteLine(word.Remove(5,2));
+
+            //Console.WriteLine(word.Normalize());
+
+            //Console.WriteLine(word.Contains("Hel"));
+
+            //Console.WriteLine(word.Equals();
+
+            int[] nums = { 12, 57, 13 };
+            //Console.WriteLine(nums.GetValue(0));
+
+
+            //Array.Reverse(nums);
+            //foreach(int i in nums)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+        }
+    }
+}
